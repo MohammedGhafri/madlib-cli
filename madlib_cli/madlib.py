3e025fb9b5a2c5ccb5a744ab1b6f5ea02b265280
mohammedghafri <eng.m.ghafri@gmail.com>
2020-08-18 22:16:33 +0300

    with write to file

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,21 +1,33 @@
 import re
 arrayForInput=[]
+
+
+# function for inputs
 def fin(x):
     a=input()
     arrayForInput.append(a)
     
     return arrayForInput[x] 
+
+
+
 a=6
+# tp open the text.txt file
 with open('assets/text.txt') as f:
     content=f.read()
+
+
 content
+# to change the changable words with %s to do the string format
 x=re.sub("{\w*}","%s",content)
+
+# to store the changable words into list
 words=re.findall("{\w*}",content)
 # print(x,words)
 
 
 
-
+# for test purposes
 def parse(txt):
     words=re.findall("{\w*}",txt)
     str1 = ''.join(words)
@@ -23,24 +35,30 @@ def parse(txt):
 
 # print(parse(content))
 
+
+# for test purposes
+
 def  read_template(txt):
     return txt.read().strip()
 
 
+
+
+# an empty array to store input from user
 emp_arr=[]   
 
-
-
-
+# function to do the input from user and show the final result
 def fun_input(statement):
 
     for i in words:
         remove_char=re.sub("{|}",'',i)
         
         emp_arr.append(input("\n\nPlease Enter a/an %s  : "%(remove_char)))
+    with open('assets/user_text.txt','w') as f2:
+        f2.write(x % tuple(emp_arr))
     return (x % tuple(emp_arr))
 
-# print(fun_input(words))
+print(fun_input(words))
 
 def fun_input_test(statement,entries):
     # input_arr=["mohammed","study","alone","computer","home","sleepy","table","mouse","laptop"]
@@ -48,6 +66,7 @@ def fun_input_test(statement,entries):
     x=re.sub("{\w*}","%s",statement)
     # print(x)
     return (x % entries)
+
 
 
 # print(fun_input_test(open('assets/text.txt').read(),aaaa))
